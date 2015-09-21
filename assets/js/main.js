017ea7625de2acd01cf2a02a4cf58eea5cb57cb1
kodermax <kodermax@gmail.com>
2015-09-21 19:59:08 +0300

    phone validate

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,19 @@ $().ready(function(){
         this.classList.toggle('open');
     });
     $('.phone input').inputmask('+7(999)999-99-99',{"placeholder": "_",clearMaskOnLostFocus: false});
-        $('.form_phone').on('ajaxSuccess', function(ev, context, data, status, jqXHR) {
+    var formPhone = $('.form_phone');
+    formPhone.submit(function() {
+        var phone = formPhone.find('.phone input').val();
+        phone = phone.replace(/[^0-9]/g, '');
+        if(phone.length !== 11) {
+            alert('Не верно указан телефон!');
+            return false;
+        }
+        else {
+            return true;
+        }
+    });
+    formPhone.on('ajaxSuccess', function(ev, context, data, status, jqXHR) {
             if (data.error === true) {
                 return console.log(arguments);
             } else {
