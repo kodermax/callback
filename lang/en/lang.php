1fb4f582e97025936564d7cb9eb8ad636d84f524
kodermax <kodermax@gmail.com>
2015-12-02 23:01:22 +0300

    english language

---
+++ lang/en/lang.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    'messages' => [
+        'phone' => [
+            'required' => 'The phone is required!'
+        ]
+    ],
+    'component' => [
+        'name' => 'CallBack',
+        'description' => 'Show CallBack Form'
+    ],
+    'plugin' => [
+        'name' => 'CallBack',
+        'description' => 'Create CallBack Form'
+    ],
+    'settings' => [
+        'label' => 'Settings CallBack',
+        'description' => 'Manage settings',
+    ],
+    'email' => [
+        'message' => 'Send message administrator'
+    ],
+    'menu' => [
+        'label' => 'CallBack'
+    ]
+];
+?>
